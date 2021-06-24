9c9371d6674af5c138c8916bd25ba96453216c81
Chen <khang@bacoor.co>
2021-06-24 09:21:45 +0700

    fix bug

--- pages/Screen/Consignment/components/SearchForm/index.js
+++ pages/Screen/Consignment/components/SearchForm/index.js
@@ -228,9 +228,8 @@ class SearchForm extends React.PureComponent {
             <Descriptions.Item span={24} label='Số lượng đã bán'>{item.numSoldConsignment}</Descriptions.Item>
             <Descriptions.Item span={24} label='Số lượng còn lại'>{item.remainNumConsignment}</Descriptions.Item>
             <Descriptions.Item span={24} label='Ngân hàng đăng ký'>{item.banks[0].type}</Descriptions.Item>
+            <Descriptions.Item span={24} label='ID ngân hàng'>{item.banks[0].accNumber}</Descriptions.Item>
             <Descriptions.Item span={24} label='Nhận tiền'>{item.isTransferMoneyWithBank ? 'Chuyển khoản' : 'Trực tiếp   '}</Descriptions.Item>
-
-            <Descriptions.Item span={24} label='Số lượng còn lại'>{item.banks[0].accNumber}</Descriptions.Item>
             <Descriptions.Item span={24} label='Tổng tiền'>{numberWithCommas(item.moneyBack)} vnd</Descriptions.Item>
             <Descriptions.Item span={24} label='Ngày tổng kết'>{item.timeGetMoney}</Descriptions.Item>
 
