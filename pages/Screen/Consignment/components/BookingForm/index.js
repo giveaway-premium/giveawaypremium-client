b0191ac4cd57ed86fc582f9ca100727659ad294f
Chen <khang@bacoor.co>
2022-05-25 20:54:09 +0700

    fix

--- pages/Screen/Consignment/components/BookingForm/index.js
+++ pages/Screen/Consignment/components/BookingForm/index.js
@@ -121,9 +121,9 @@ class ConsignmentScreen extends React.PureComponent {
       else if (bookingOptionData.OPTION_6.includes(choosenDayCode.dayCode)) return { option: 6, timeBooking: TIME_BOOKING.OPTION_6 }
       else if (bookingOptionData.OPTION_7.includes(choosenDayCode.dayCode)) return { option: 7, timeBooking: TIME_BOOKING.OPTION_7 }
       else if (bookingOptionData.OPTION_8.includes(choosenDayCode.dayCode)) return { option: 8, timeBooking: TIME_BOOKING.OPTION_8 }
-      else return { option: 8, timeBooking: TIME_BOOKING.OPTION_2 }
+      else return { option: 8, timeBooking: TIME_BOOKING.OPTION_8 }
     } else {
-      return { option: 8, timeBooking: TIME_BOOKING.OPTION_2 }
+      return { option: 8, timeBooking: TIME_BOOKING.OPTION_8 }
     }
   }
 
