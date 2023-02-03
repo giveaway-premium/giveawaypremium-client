0a4096d45b3bb3f3908ca7332323aac800a74d10
Chen <khang@bacoor.co>
2023-02-03 14:29:57 +0700

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -146,11 +146,10 @@ class GiveAway extends App {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-              
-                gtag('config', 'G-ZYZJ59HWQE');
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-ZYZJ59HWQE');
               `
             }}
           />
